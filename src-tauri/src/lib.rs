d5c05817bcb3d6109466602479185dc581cb5442
Ben Faerber <benfaerber@protonmail.com>
2026-01-02 23:27:38 -0700

    Basic app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,77 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::{
+    menu::{MenuBuilder, MenuItemBuilder},
+    tray::{TrayIconBuilder, TrayIconEvent},
+    Manager,
+};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from Rust!!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            if let Some(window) = app.get_webview_window("main") {
+                let icon_bytes = include_bytes!("../icons/icon.png");
+                if let Ok(icon) = image::load_from_memory(icon_bytes) {
+                    let rgba = icon.to_rgba8();
+                    let (width, height) = rgba.dimensions();
+                    let tauri_icon = tauri::image::Image::new_owned(rgba.into_raw(), width, height);
+                    let _ = window.set_icon(tauri_icon);
+                }
+            }
+
+            let show_item = MenuItemBuilder::with_id("show", "Open Claude").build(app)?;
+            let quit_item = MenuItemBuilder::with_id("quit", "Quit").build(app)?;
+            let menu = MenuBuilder::new(app)
+                .item(&show_item)
+                .item(&quit_item)
+                .build()?;
+
+            let tray_icon_bytes = include_bytes!("../icons/tray-icon.png");
+            let tray_image = image::load_from_memory(tray_icon_bytes).unwrap();
+            let tray_rgba = tray_image.to_rgba8();
+            let (tray_width, tray_height) = tray_rgba.dimensions();
+            let tray_icon =
+                tauri::image::Image::new_owned(tray_rgba.into_raw(), tray_width, tray_height);
+
+            let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .icon(tray_icon)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "show" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: tauri::tray::MouseButton::Left,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                })
+                .build(app)?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
